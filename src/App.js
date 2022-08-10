905e9cd5609813dd2f659ef5fe99c33f1cdf2ba6
YohanKael <yohan_garcia4@hotmail.com>
2022-08-10 19:57:43 -0300

    basic html structure

--- src/App.js
+++ src/App.js
@@ -2,7 +2,21 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      <div className='Header'>
+        Dashboard
+        Desafio Técnico Frontend
+        <button>Filtrar</button>
+      </div>
+      <div className='Graficos'>
+        <div className='GraficoBarras'>
+          <div>Barras</div>
+        </div>
+        <div className='GraficoScatter'>
+          <div>Scatter</div>
+        </div>
+      </div>
+    </div>
   );
 }
 
