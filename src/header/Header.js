a7ad626db0e43751a69ab9a6cd0de9dcb5090757
YohanKael <yohan_garcia4@hotmail.com>
2022-08-15 21:15:30 -0300

    implemented filter button

--- src/header/Header.js
+++ src/header/Header.js
@@ -2,19 +2,33 @@ import React from 'react';
 import { FaFilter } from 'react-icons/fa';
 import './Header.css';
 
-const Header = () => {
+const Header = ({chartsInfo, handleDropdownChange}) => {
   return (
     <div className='Header'>
-        <div className='HeaderTitle'>
-          <h1>Dashboard</h1>
-          <h2>Desafio Técnico Frontend</h2>
-        </div>
+      <div className='HeaderTitle'>
+        <h1>Dashboard</h1>
+        <h2>Desafio Técnico Frontend</h2>
+      </div>
+      <div className='dropdown'>
         <div className='HeaderButton'>
-          <FaFilter size={"1rem"}/>
+          <FaFilter size={"1rem"} />
           <p>Filtrar</p>
         </div>
+        <div className='dropdown-content'>
+          {chartsInfo.map((info, i) => <DropDownItem key={i} title={info.options.title.text} show={info.show} handleDropdownChange={handleDropdownChange}/>)}
+        </div>
       </div>
+    </div>
   )
 };
 
+const DropDownItem = ({title, show, handleDropdownChange}) => {
+  return (
+  <label className="container">
+    <input type="checkbox" defaultChecked={show} onChange={() => handleDropdownChange(title)}/>
+    <span className="checkmark"></span>
+    {title}
+  </label>
+  )
+}
 export default Header;
